81a4a70afba10a22adcf54a7510252f653065032
Dara2004 <dara.nguyenthuy@gmail.com>
2016-12-11 08:45:23 -0800

    Add files via upload

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,34 +1,17 @@
 # > caesar_cipher("What a string!", 5)
 # => "Bmfy f xywnsl!"
-# collect user's input
 def caesar_cipher(string, key)
-    alphabet = ('a'..'z').to_a * 2  #this is to wrap z to a
-    puts "Enter a string: "
-    string = gets.chomp
+    alphabet = ('a'..'z').to_a * 2  #wrap z to a
     letters = string.split("")
-    p letters  #an arrays of letters
-    puts "Enter a key: "
-    key = gets.chomp.to_i  #worked
+    # p letters
 
-# assign to each letter a numeber based on alphabet index + the key
-    numbers = letters.map do |letter|
-      # a space also has string class, NOT nil
-      if alphabet.index(letter) == nil
-        next
+    letters.map do |letter|
+      if alphabet.include? (letter.downcase)
+        key.times {letter = letter.next}
       end
-        alphabet.index(letter) + key
+      print letter[-1]
     end
-    p numbers #numbers = [8, 23, 5, 10]
-
-# assign numbers back to letters to the alphabet
-    # reversed_alphabet = alphabet.reverse # wrap from z to a
-    result = numbers.map do |number|
-      if number == nil
-        next
-        puts " "
-      end
-        alphabet[number]
-    end
-    p result.join   #result = ["b", "m", "f", "y", nil, "f"]
 end
 caesar_cipher("What a string!", 5)
+caesar_cipher("Testing", 4)
+# Bmfy f xywnsl!
